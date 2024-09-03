9aac4348ba87a3183cb77694320901af055e46a2
nipun-dezoysa <nipunavishka123@gmail.com>
2024-09-04 00:54:50 +0530

    Create LoginDto.cs

---
+++ Api/Api/Dtos/LoginDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Api.Dtos
+{
+    public record class LoginDto
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
