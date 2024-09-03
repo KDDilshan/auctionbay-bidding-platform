9e8312fcc99afaace1d280670f5935bed1ce61b3
nipun-dezoysa <nipunavishka123@gmail.com>
2024-09-04 00:12:43 +0530

    user roles added

---
+++ Api/Api/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using Api.Data;
+using Api.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly IConfiguration _configuration;
+        private readonly AppDbContext _context;
+
+        public UserController(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration, AppDbContext context)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+            _context = context;
+        }
+
+
+    }
+}
--- Api/Api/Data/AppDbContext.cs
+++ Api/Api/Data/AppDbContext.cs
@@ -1,6 +1,27 @@
-﻿namespace Api.Data
+﻿using Api.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Data
 {
-    public class AppDbContext
+    public class AppDbContext:IdentityDbContext<AppUser>
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Seed roles
+            builder.Entity<IdentityRole>().HasData(
+                new IdentityRole { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole { Id = "2", Name = "Buyer", NormalizedName = "BUYER" },
+                new IdentityRole { Id = "3", Name = "Seller", NormalizedName = "SELLER" }
+            );
+        }
     }
 }
