0da54e795492dcd686732b8315b05e7b2de8f434
nipun-dezoysa <nipunavishka123@gmail.com>
2024-09-04 21:38:18 +0530

    unnecessary variable deleted

--- Api/Api/Controllers/UserController.cs
+++ Api/Api/Controllers/UserController.cs
@@ -56,7 +56,6 @@ namespace Api.Controllers
             {
                 return Unauthorized(new AuthResponseDto
                 {
-                    IsSuccess = false,
                     Message = "User not found with this email",
                 });
             }
@@ -67,7 +66,6 @@ namespace Api.Controllers
             {
                 return Unauthorized(new AuthResponseDto
                 {
-                    IsSuccess = false,
                     Message = "Invalid Password."
                 });
             }
@@ -76,7 +74,6 @@ namespace Api.Controllers
             return Ok(new AuthResponseDto
             {
                 Token = token,
-                IsSuccess = true,
                 Message = "Login Success."
             });
         }
--- Api/Api/Dtos/AuthResponseDto.cs
+++ Api/Api/Dtos/AuthResponseDto.cs
@@ -3,7 +3,6 @@
     public class AuthResponseDto
     {
         public string? Token { get; set; } = string.Empty;
-        public bool IsSuccess { get; set; }
         public string? Message { get; set; }
     }
 }
