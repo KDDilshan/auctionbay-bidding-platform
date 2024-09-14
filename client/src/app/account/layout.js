156df17c93d3497397e22b408ec2497dfb8a35a2
nipun-dezoysa <nipunavishka123@gmail.com>
2024-09-14 13:57:37 +0530

    account navigation current route indicator

--- client/src/app/account/layout.js
+++ client/src/app/account/layout.js
@@ -1,3 +1,4 @@
+"use client";
 import { Link } from "@nextui-org/react";
 import React from "react";
 import {
@@ -7,13 +8,23 @@ import {
   FaBoxOpen,
   FaStoreAlt,
 } from "react-icons/fa";
+import { usePathname } from "next/navigation";
 function layout({ children }) {
+  const pathname = usePathname();
   const links = [
     { name: "Account Settings", icon: <FaUserAlt />, href: "/account" },
-    { name: "Email Preferences", icon: <FaBell />, href: "/account" },
-    { name: "Transactions", icon: <FaWallet />, href: "/account" },
-    { name: "Inventory", icon: <FaBoxOpen />, href: "/account" },
-    { name: "Marketplace Seller", icon: <FaStoreAlt />, href: "/account" },
+    {
+      name: "Email Preferences",
+      icon: <FaBell />,
+      href: "/account/notifiactions",
+    },
+    { name: "Transactions", icon: <FaWallet />, href: "/account/transactions" },
+    { name: "Inventory", icon: <FaBoxOpen />, href: "/account/inventory" },
+    {
+      name: "Marketplace Seller",
+      icon: <FaStoreAlt />,
+      href: "/account/seller",
+    },
   ];
   return (
     <div className="container-full">
@@ -22,7 +33,12 @@ function layout({ children }) {
           {links.map((link) => (
             <Link
               href={link.href}
-              className="flex px-[15px] py-[7px] gap-3 hover:bg-zinc-800 text-zinc-500 hover:text-white transition duration-200 ease-in-out"
+              className={
+                "flex px-[15px] py-[7px] gap-3 transition duration-200 ease-in-out" +
+                (pathname === link.href
+                  ? " bg-zinc-800 text-white"
+                  : " hover:bg-zinc-800 text-zinc-500 hover:text-white")
+              }
             >
               <div className="w-[40px] h-[40px] flex items-center justify-center text-lg">
                 {link.icon}
@@ -32,6 +48,7 @@ function layout({ children }) {
           ))}
         </div>
         <div className="bg-zinc-900 sm:flex-1 p-8 rounded-md border border-zinc-700">
+          <h1>{pathname}</h1>
           {children}
         </div>
       </div>
