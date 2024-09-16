a551696effd8787f7ea1bd029f9f1d06cd201a37
Dinesh Rathnasiri <dineshrathnasiri017@gmail.com>
2024-09-16 13:27:13 +0530

    hero sction
    adding image and hi and p

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,12 +1,16 @@
 "use client";
 
+import Hero from "@/components/Hero";
 import ShowCase from "@/components/ShowCase";
 
 export default function Home() {
   return (
+    <>
     <main className="flex flex-col gap-5">
+      <Hero/>
       <ShowCase />
       <ShowCase />
     </main>
+    </>
   );
 }
