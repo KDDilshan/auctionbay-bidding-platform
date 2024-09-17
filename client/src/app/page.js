756c70ba42131a6fba0267e505df221e48ae60d9
Dinesh Rathnasiri <dineshrathnasiri017@gmail.com>
2024-09-17 15:03:54 +0530

    top list creators

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,8 +1,10 @@
 "use client";
 
 import Categories from "@/components/Categories";
+import Creators from "@/components/Creators";
 import Hero from "@/components/Hero";
 import ShowCase from "@/components/ShowCase";
+import CreatorCard from "@/components/CreatorCard";
 
 export default function Home() {
   return (
@@ -10,6 +12,7 @@ export default function Home() {
     <main className="flex flex-col gap-5">
       <Hero/>
       <Categories/>
+      <Creators />
       <ShowCase />
       <ShowCase />
     </main>
