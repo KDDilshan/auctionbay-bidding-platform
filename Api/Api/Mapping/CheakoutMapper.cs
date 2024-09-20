9f6973286c3e54c037c6f0a4786f7be64104c015
KDDilshan <kdprojects847@gmail.com>
2024-09-20 19:37:45 +0530

    added new mapping profile with auto mapper

--- Api/Api/Mapping/CheakoutMapper.cs
+++ Api/Api/Mapping/CheakoutMapper.cs
@@ -10,11 +10,22 @@ namespace Api.Mapping
         {
             CreateMap<Auction, CheakoutDto>()
                 .ForMember(dest => dest.NftTitle, opt => opt.MapFrom(src => src.Nft.Title))
-                .ForMember(dest => dest.NftDescription, opt => opt.MapFrom(src => src.Nft.description))
-                .ForMember(dest => dest.FinalPrice, opt => opt.MapFrom(src => src.Bids.OrderByDescending(b => b.BidPrice).FirstOrDefault().BidPrice))
-                .ForMember(dest => dest.BuyerId, opt => opt.MapFrom(src => src.Bids.OrderByDescending(b => b.BidPrice).FirstOrDefault().UserId))
-                .ForMember(dest => dest.BuyerId, opt => opt.MapFrom(src => src.Bids.OrderByDescending(b => b.BidPrice).FirstOrDefault().UserId));
+                .ForMember(dest => dest.NftDescription, opt => opt.MapFrom(src => src.Nft.Description))
+                .ForMember(dest => dest.FinalPrice, opt => opt.MapFrom(src =>
+                    src.Bids.OrderByDescending(b => b.BidPrice).FirstOrDefault() != null
+                        ? src.Bids.OrderByDescending(b => b.BidPrice).First().BidPrice
+                        : 0)) // Default to 0 if there are no bids
+                .ForMember(dest => dest.BuyerId, opt => opt.MapFrom(src =>
+                    src.Bids.OrderByDescending(b => b.BidPrice).FirstOrDefault() != null
+                        ? src.Bids.OrderByDescending(b => b.BidPrice).First().UserId
+                        : null)) // Default to null if there are no bids
+                .ForMember(dest => dest.BuyerUsername, opt => opt.MapFrom(src =>
+                    src.Bids.OrderByDescending(b => b.BidPrice).FirstOrDefault() != null
+                        ? src.Bids.OrderByDescending(b => b.BidPrice).First().AppUsers.FirstName 
+                        : null));
 
-        }
+
+            
+        }   
     }
 }
