c9b2f71cf56df4cd4ec7c59528f7995cd3a54e4e
KDDilshan <kdprojects847@gmail.com>
2024-09-22 22:47:22 +0530

    added new dto

--- Api/Api/Dtos/NftDto.cs
+++ Api/Api/Dtos/NftDto.cs
@@ -2,11 +2,11 @@
 {
     public class NftDto
     {
-        public int Id { get; set; }
-
         public string Title { get; set; }
 
         public string Description { get; set; }
+
+        public string UserId { get; set; }
 
     }
 }
