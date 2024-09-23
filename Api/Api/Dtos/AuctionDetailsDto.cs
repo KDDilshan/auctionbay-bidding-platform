ae3cfc455bf4716517539d0a1b26da1e6a9d339f
KDDilshan <kdprojects847@gmail.com>
2024-09-23 23:18:19 +0530

    added the dto geting reqired data

---
+++ Api/Api/Dtos/AuctionDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Api.Dtos
+{
+    public class AuctionDetailsDto
+    {
+        public string NftTitle { get; set; }
+
+        public string FirstBidAmount { get; set; }
+
+        public int NumberOfBids {  get; set; }
+
+        public long HighestBid {  get; set; }
+
+        public string TimeRemaining { get; set; }
+    }
+}
