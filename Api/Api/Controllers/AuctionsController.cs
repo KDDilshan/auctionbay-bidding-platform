259e785524dfe435ec101044b319c94eca83f50e
KDDilshan <kdprojects847@gmail.com>
2024-09-23 23:17:56 +0530

    added new controller for geting aution details

---
+++ Api/Api/Controllers/AuctionsController.cs
@@ -0,0 +1,26 @@
+﻿using Api.Services.AuctionService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuctionsController : ControllerBase
+    {
+        public readonly AuctionService _auctionService;
+
+        public AuctionsController(AuctionService auctionService)
+        {
+            _auctionService = auctionService;
+        }
+
+        [HttpGet("all")]
+        public async Task<IActionResult> GetAllAuctionDetils()
+        {
+            var auctions=await _auctionService.GetAuctionDetailsAsync();
+            return Ok(auctions);
+
+        }
+    }
+}
