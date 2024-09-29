c27d1ca921e93e6b7e3f35daf91a37db005aaaf7
nipun-dezoysa <nipunavishka123@gmail.com>
2024-09-29 21:11:17 +0530

    refactor: move get user info function from header to provider

--- client/src/app/providers.js
+++ client/src/app/providers.js
@@ -1,13 +1,33 @@
 "use client";
+import { apiLink, getToken } from "@/configs";
 import { NextUIProvider } from "@nextui-org/react";
 import { useRouter } from "next/navigation";
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext({});
 
 export function Providers({ children }) {
   const [userInfo, setUserInfo] = useState(null);
   const router = useRouter();
+
+  useEffect(() => {
+    var token = getToken();
+    if (token) {
+      axios
+        .get(apiLink + "/api/User", {
+          headers: { Authorization: token },
+        })
+        .then((res) =>
+          setUserInfo({
+            email: res.data.email,
+            firstName: res.data.firstName,
+            lastName: res.data.lastName,
+          })
+        )
+        .catch((er) => console.log(er));
+    }
+  }, []);
+
   return (
     <NextUIProvider navigate={router.push}>
       <UserContext.Provider value={{ userInfo, setUserInfo }}>
