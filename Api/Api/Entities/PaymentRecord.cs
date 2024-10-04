432383fcb1b36fbec0254372b1b9cd8bcfa341b8
KDDilshan <kdprojects847@gmail.com>
2024-10-04 21:17:31 +0530

    added new table for payment recoreds

---
+++ Api/Api/Entities/PaymentRecord.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api.Entities
+{
+    public class PaymentRecord
+    {
+        [Key]
+        public string StripeSessionId { get; set; }
+
+        [ForeignKey("User")]
+        public string UserId { get; set; }
+
+        [ForeignKey("Auction")]
+        public int AuctionId { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public string Status { get; set; }
+
+        public virtual AppUser User { get; set; }
+        public virtual Auction Auction { get; set; }
+    }
+
+
+}
