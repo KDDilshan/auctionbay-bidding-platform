92190737309f591172c5d83a89f87eb3b183d188
nipun-dezoysa <nipunavishka123@gmail.com>
2024-10-09 23:47:37 +0530

    feat: add seller request response

---
+++ Api/Api/Dtos/SellerRequestResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Api.Dtos
+{
+    public class SellerRequestResponse
+    {
+        public int? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public DateTime? Created { get; set; }
+        public string? Address { get; set; }
+        public DateTime? Dob { get; set; }
+        public string? Status { get; set; }
+    }
+}
