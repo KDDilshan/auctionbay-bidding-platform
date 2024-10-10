5361cdb2fda0acbd93ad4d92c6b23ecdfd5dd5bf
nipun-dezoysa <nipunavishka123@gmail.com>
2024-10-10 23:46:48 +0530

    feat: add Dashboard components

---
+++ client/src/components/Dashboard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import DashboardMenu from "./DashboardMenu";
+import DashboardHeader from "./DashboardHeader";
+function Dashboard({ children, links, name }) {
+  return (
+    <div className="container-w flex h-screen grow fixed top-0">
+      <DashboardMenu links={links} name={name} />
+      <div className="w-full lg:w-[80%] h-full">
+        <DashboardHeader links={links} />
+        <div className="w-full h-full bg-zinc-950 overflow-y-auto overflow-x-hidden p-5">
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
