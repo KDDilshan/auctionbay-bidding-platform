8be2be58f0b835aa17ef8e6d4192cf3b50619543
nipun-dezoysa <nipunavishka123@gmail.com>
2024-10-12 12:36:43 +0530

    feat: add create auction endpoint

---
+++ Api/Api/Dtos/AuctionDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Api.Dtos
+{
+    public record class AuctionDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public long Price { get; set; }
+        public int NftId { get; set; }
+    }
+}
---
+++ Api/Api/Mapping/AuctionMap.cs
@@ -0,0 +1,22 @@
+﻿using Api.Dtos;
+using Api.Entities;
+
+namespace Api.Mapping
+{
+    public static class AuctionMap
+    {
+        public static Auction ToEntity(this AuctionDto auction,string UserId)
+        {
+            return new Auction
+            {
+                Title = auction.Title,
+                Description = auction.Description,
+                StartDate = auction.StartDate,
+                EndDate = auction.EndDate,
+                Price = auction.Price,
+                NftId = auction.NftId,
+                UserID = UserId
+            };
+        }
+    }
+}
