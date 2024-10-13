bb325e0e69993af0bec8228f930b27b3870ed785
nipun-dezoysa <nipunavishka123@gmail.com>
2024-10-13 20:57:22 +0530

    Update next.config.mjs

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "localhost",
+        port: "7218",
+        pathname: "/wwwroot/uploads/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
