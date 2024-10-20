298ca7e53351f1a7d5fb1cb5c71222160e951917
nipun-dezoysa <nipunavishka123@gmail.com>
2024-10-20 21:57:43 +0530

    feat: add backend integration to showcases

--- client/src/components/ItemCard.js
+++ client/src/components/ItemCard.js
@@ -1,13 +1,15 @@
 "use client";
-import { Image } from "@nextui-org/react";
+import { apiLink, formatCurrency } from "@/configs";
+import { useRouter } from "next/navigation";
 import React from "react";
 import Countdown from "react-countdown";
 
-function ItemCard() {
+function ItemCard({ item }) {
+  const router = useRouter();
   const renderer = ({ days, hours, minutes, seconds, completed }) => {
     if (completed) {
       // Render a completed state
-      return <div>SOLD</div>;
+      return <div>{item.status == "Open" ? "Closed" : item.status}</div>;
     } else {
       // Render a countdown
       return (
@@ -26,34 +28,32 @@ function ItemCard() {
   function formatTime(time) {
     return time < 10 ? `0${time}` : time;
   }
+
   return (
-    <div className="bg-zinc-900 transition ease-in-out duration-300 hover:bg-zinc-800 relative hover:bottom-2 w-[280px] shrink-0 grow-0 rounded-xl overflow-hidden">
+    <div
+      onClick={() => router.replace("/auction/" + item.id)}
+      className="bg-zinc-900 transition ease-in-out duration-300 hover:bg-zinc-800 relative hover:bottom-2 w-[280px] shrink-0 grow-0 rounded-xl overflow-hidden"
+    >
       <div className="w-full h-[170px] overflow-hidden">
-        <Image
-          radius="none"
-          removeWrapper
-          isZoomed
-          classNames={{
-            img: "object-cover w-full h-full",
-          }}
-          loading="lazy"
-          src="https://i.seadn.io/gcs/files/cf409610bec578435f454b9cf78da72f.png?auto=format&dpr=1&h=500"
+        <img
+          src={apiLink + "/wwwroot/uploads/" + item.image}
+          className="object-cover w-full h-full hover:scale-105 transition ease-in-out duration-300"
+          alt={item.title}
         />
       </div>
       <div className="p-4 flex flex-col gap-1">
-        <h1 className=" font-bold">Blast Royale: Corpos</h1>
-        <div className="text-sm text-zinc-300 flex justify-between">
-          <p>USDT 300.00</p>
-          <p>12 Bids</p>
-        </div>
+        <h1 className=" font-bold">{item.title}</h1>
         <div className="flex  justify-between">
           <div>
             <div className="text-sm  text-zinc-400">Time Left</div>
-            <Countdown renderer={renderer} date={Date.now() + 100000000} />
+            <Countdown
+              renderer={renderer}
+              date={new Date(item.endDate.slice(0, 23) + "Z").toLocaleString()}
+            />
           </div>
           <div className="flex flex-col items-end">
             <div className="text-sm text-zinc-400">Current Bid</div>
-            <div>USD 3000.00</div>
+            <div>{formatCurrency(item.currentBid)}</div>
           </div>
         </div>
       </div>
--- client/src/components/ShowCase.js
+++ client/src/components/ShowCase.js
@@ -1,11 +1,17 @@
-'use client';
-import React from "react";
-import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+"use client";
+import React, { useEffect, useState } from "react";
 import ItemCard from "./ItemCard";
 import useEmblaCarousel from "embla-carousel-react";
-import { NextButton, PrevButton, usePrevNextButtons } from "./EmblaCarouselArrowButtons";
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons,
+} from "./EmblaCarouselArrowButtons";
+import axios from "axios";
+import { apiLink, getToken } from "@/configs";
 
-function ShowCase() {
+function ShowCase({ title, category }) {
+  const [items, setItems] = useState([]);
   const [emblaRef, emblaApi] = useEmblaCarousel({
     align: "start",
   });
@@ -15,31 +21,28 @@ function ShowCase() {
     onPrevButtonClick,
     onNextButtonClick,
   } = usePrevNextButtons(emblaApi);
-  return (
+  useEffect(() => {
+    axios
+      .get(apiLink + "/api/Auctions/category/" + category)
+      .then((res) => setItems(res.data))
+      .catch((er) => console.log(er));
+  }, []);
+  return items.length > 0 ? (
     <section className="container-w flex flex-col gap-1">
-      <h1 className="max-lg:px-6 lg:px-8 text-2xl font-bold">
-        Top Collector Buys Today
-      </h1>
+      <h1 className="max-lg:px-6 lg:px-8 text-2xl font-bold">{title}</h1>
       <div className="w-full lg:px-8 flex relative group/show">
         <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
         <div className="embla__viewport max-lg:px-6" ref={emblaRef}>
           <div className="embla__container gap-4 py-2">
-            <ItemCard key={1} />
-            <ItemCard key={2} />
-            <ItemCard key={3} />
-            <ItemCard key={4} />
-            <ItemCard key={5} />
-            <ItemCard key={6} />
-            <ItemCard key={7} />
-            <ItemCard key={8} />
-            <ItemCard key={9} />
-            <ItemCard key={10} />
+            {items.map((item, index) => (
+              <ItemCard key={item.id} item={item} />
+            ))}
           </div>
         </div>
         <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
       </div>
     </section>
-  );
+  ) : null;
 }
 
 export default ShowCase;
