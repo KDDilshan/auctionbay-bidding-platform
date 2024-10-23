9cdabc7b288955df825b89d3c699e95666d0fa12
nipun-dezoysa <nipunavishka123@gmail.com>
2024-10-24 02:05:06 +0530

    feat: create custom Table component

---
+++ client/src/components/Table.js
@@ -0,0 +1,30 @@
+"use client";
+import React from "react";
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+} from "@nextui-org/react";
+function MyTable({ columns, rows, renderCell, emptyContent }) {
+  return (
+    <Table>
+      <TableHeader columns={columns}>
+        {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
+      </TableHeader>
+      <TableBody emptyContent={emptyContent} items={rows}>
+        {(item) => (
+          <TableRow key={item.key}>
+            {(columnKey) => (
+              <TableCell>{renderCell(item, columnKey)}</TableCell>
+            )}
+          </TableRow>
+        )}
+      </TableBody>
+    </Table>
+  );
+}
+
+export default MyTable;
