d8c23947020cfb9cb890bea48ebfdc2305a2ed61
nipun-dezoysa <nipunavishka123@gmail.com>
2024-10-24 01:34:59 +0530

    feat: add only can access admin rule for admin pages

--- client/src/app/(admin)/admin/layout.js
+++ client/src/app/(admin)/admin/layout.js
@@ -1,8 +1,22 @@
 "use client";
 import { MdDashboard } from "react-icons/md";
 import Dashboard from "@/components/Dashboard";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "@/app/providers";
+import { useRouter } from "next/navigation";
+import Loading from "@/components/Loading";
 
 export default function RootLayout({ children }) {
+  const { userInfo } = useContext(UserContext);
+  const router = useRouter();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!userInfo) router.replace("/login");
+    else if (userInfo.role !== "Admin") router.replace("/login");
+    else setLoading(false);
+  }, []);
+
   const links = [
     { name: "Dashboard", icon: <MdDashboard />, href: "/admin" },
     {
@@ -12,6 +26,11 @@ export default function RootLayout({ children }) {
     },
     { name: "Users", icon: <MdDashboard />, href: "/admin/users" },
   ];
+
+  if (loading) {
+    return <Loading />;
+  }
+
   return (
     <Dashboard links={links} name={"Admin Dashboard"}>
       {children}
