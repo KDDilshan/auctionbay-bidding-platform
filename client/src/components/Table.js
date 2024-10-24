b16dda1174b82fd865383b007ab60c46a6f6dd3d
nipun-dezoysa <nipunavishka123@gmail.com>
2024-10-25 01:41:50 +0530

    feat: add loading screen for table component

--- client/src/components/Table.js
+++ client/src/components/Table.js
@@ -7,14 +7,20 @@ import {
   TableBody,
   TableRow,
   TableCell,
+  Spinner,
 } from "@nextui-org/react";
-function MyTable({ columns, rows, renderCell, emptyContent }) {
+function MyTable({ columns, rows, renderCell, emptyContent, isLoading }) {
   return (
     <Table>
       <TableHeader columns={columns}>
         {(column) => <TableColumn key={column.key}>{column.label}</TableColumn>}
       </TableHeader>
-      <TableBody emptyContent={emptyContent} items={rows}>
+      <TableBody
+        emptyContent={emptyContent}
+        isLoading={isLoading}
+        loadingContent={<Spinner color="default" label="Loading..." />}
+        items={rows}
+      >
         {(item) => (
           <TableRow key={item.key}>
             {(columnKey) => (
